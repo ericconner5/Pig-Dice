03179b74a20c5c3afd1a40dfd4d41b680df1d9a6
Eric Conner and Evan Filkins <evan@>
2018-08-22 11:21:18 -0700

    add roll button logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ var d6 = function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var player1Rolls = []
 
+var player2Rolls = []
 // var turnTotal =
 //
 // var score =
@@ -16,18 +18,33 @@ $(document).ready(function(){
     $(".hidden").show();
     $("button#play").hide();
   })
+  // var rollSum =
 
 
-
-
-
-
-    $("#player-1-roll-button").click(function(){
-    $("#player-1-roll-span").text(d6);
+  $("#player-1-roll-button").click(function(){
+    var roll = d6(1,6)
+    $("#player-1-roll-span").text(roll);
+    (player1Rolls).push(roll);
   })
 
 
   $("#player-2-roll-button").click(function(){
-    $("#player-2-roll-span").text(d6);
+    var roll = d6(1,6)
+    $("#player-2-roll-span").text(roll);
+    (player2Rolls).push(roll);
   })
+})
+
+
+
+
+
+$("#player-1-hold-button").click(function(){
+  $("#player-1-turn-span").text(roll);
+
+})
+
+$("player-2-hold-button").click(function(){
+  $("#player-2-turn-span").text(roll);
+
 })
