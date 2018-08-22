eab7364ba9e8df042a49fb690cea526076c0d094
Eric Conner and Evan Filkins <eric@>
2018-08-22 15:24:02 -0700

    add css and hold logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,47 +4,76 @@ var d6 = function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var sumPlayer1 = function() {
+  var sum = 0;
+  for (i=0; i < player1Rolls.length; i++) {
+    sum += player1Rolls[i];
+    (player1RollsSum).push(sum);
+  }
+}
+
+var sumPlayer2 = function() {
+  var sum = 0;
+  for (i=0; i < player2Rolls.length; i++) {
+    sum += player2Rolls[i];
+    (player2RollsSum).push(sum);
+  }
+}
+
+
+
 var player1Rolls = []
 
 var player2Rolls = []
-// var turnTotal =
-//
-// var score =
 
-// var roll =
+var player1RollsSum = []
+
+var player2RollsSum = []
+
+
 
 $(document).ready(function(){
   $("button#play").click(function(){
     $(".hidden").show();
     $("button#play").hide();
   })
-  // var rollSum =
 
 
   $("#player-1-roll-button").click(function(){
     var roll = d6(1,6)
     $("#player-1-roll-span").text(roll);
-    (player1Rolls).push(roll);
-  })
+    if (roll === 1){
 
+      alert("Turns up!")
+  } else {
+      (player1Rolls).push(roll);
+
+  }
+
+})
 
   $("#player-2-roll-button").click(function(){
     var roll = d6(1,6)
     $("#player-2-roll-span").text(roll);
-    (player2Rolls).push(roll);
+    if (roll === 1){
+
+      alert("Turns up!")
+  } else {
+      (player2Rolls).push(roll);
+  }
+})
+
+
+  $("#player-1-hold-button").click(function(){
+    sumPlayer1()
+    $("#player-1-score-span").text(player1RollsSum);
+    console.log(player1RollsSum);
   })
-})
 
+  $("#player-2-hold-button").click(function(){
+    sumPlayer2()
+    $("#player-2-score-span").text(player2RollsSum);
+  })
 
-
-
-
-$("#player-1-hold-button").click(function(){
-  $("#player-1-turn-span").text(roll);
-
-})
-
-$("player-2-hold-button").click(function(){
-  $("#player-2-turn-span").text(roll);
 
 })
