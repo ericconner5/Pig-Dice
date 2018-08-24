0ec5c1bbcef9c15779f0d068f9d277a0b03db0a1
Erin Conner, Evan Filkins and Mike Donovan <mike@>
2018-08-23 17:08:43 -0700

    Style CSS, adjust Javascript. Create Game and Player objects.

--- js/scripts.js
+++ js/scripts.js
@@ -3,88 +3,139 @@ var d6 = function randomInt(min, max) {
   max = Math.floor(6);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-var sumPlayer1 = function() {
-  var sum = 0;
-  for (i=0; i < player1Rolls.length; i++) {
-    sum += player1Rolls[i];
-    (player1Tracker).push(sum);
-    if (i === player1Rolls.length - 1 ) {
-      $("#player-1-turn-span").text(sum)
-    }
-  }
+var sumArray = function(array) {
+  var sum = array.reduce(getSum)
+  console.log("sum " + sum)
+  return sum
 }
-
-var sumPlayer2 = function() {
-  var sum = 0;
-  for (i=0; i < player2Rolls.length; i++) {
-    sum += player2Rolls[i];
-    (player2Tracker).push(sum);
-    if (i === player2Rolls.length - 1 ) {
-      $("#player-2-turn-span").text(sum)
-    }
-  }
+var rolls = {
+  player1: [],
+  player2: []
 }
-
-// Functions Below aren't pushing the proper index value (the last). Fix this, then the whole site will work.
-
-var totalPlayer1 = function() {
-  for (i=i; i < player1Tracker.length; i++)
-  (player1Sum).push(player1Tracker[i])
-  console.log(player1Sum);
+var sums = {
+  player1: 0,
+  player2: 0
 }
-
-var totalPlayer2 = function() {
-
-}
-
-var player1Rolls = []
-var player2Rolls = []
-
-var player1Tracker = []
-var player2Tracker = []
-
-var player1Sum = []
-var player2Sum = []
-
 
 $(document).ready(function(){
+  game = new Game();
+  player1 = new Player();
+  player2 = new Player();
+  game.changeTurn(true)
+  $("button#play").removeClass('zoomed-out');
+  $("#title-header").removeClass('off-top');
   $("button#play").click(function(){
-    $(".hidden").show();
-    $("button#play").hide();
+    $("button#play").addClass('zoomed-out');
+    setTimeout(function(){
+      $(".main-area").removeClass('hidden')
+      $(".main-area").animate({
+        opacity: 1
+      },500)
+      $("button#play").hide();
+    },1500);
+  });
+  $('#play-again-button').click(function(){
+    console.log("click")
+    window.location.reload()
   })
+});
 
-
-  $("#player-1-roll-button").click(function(){
-    var roll = d6(1,6)
-    $("#player-1-roll-span").text(roll);
+function Player() {
+  this.index = game.players.length;
+  var playerNum = this.index+1
+  this.avatar = $('#player-'+playerNum+'-avatar')
+  // this.winImage = $('#player-'+playerNum+'-win-image')
+  this.winImage = "img/winning"+playerNum+".png"
+  this.rolls = rolls['player'+playerNum];
+  this.rollButton = $('#player-'+playerNum+'-roll-button');
+  this.holdButton = $('#player-'+playerNum+'-hold-button');
+  this.rollSpan = $("#player-"+playerNum+"-roll-span");
+  this.turnSpan = $("#player-"+playerNum+"-turn-span");
+  this.scoreSpan = $("#player-"+playerNum+"-score-span");
+  this.score = 0
+  game.players.push(this);
+  var self = this;
+  this.rollButton.click(function(){
+    var roll = d6(1,6);
+    self.rollSpan.text(roll);
     if (roll === 1){
-      alert("Turns up!")
-      return player1Rolls = []
+      self.rolls = [];
+      self.turnSpan.text("0");
+      game.changeTurn()
     } else {
-      (player1Rolls).push(roll);
-      sumPlayer1();
+      self.rolls.push(roll);
+      var sum = sumArray(self.rolls);
+      self.turnSpan.text(sum);
     }
   })
+  this.holdButton.click(function(){
+    var sum = sumArray(self.rolls)
+    self.score += sum
+    self.scoreSpan.text(self.score)
 
-  $("#player-2-roll-button").click(function(){
-    var roll = d6(1,6)
-    $("#player-2-roll-span").text(roll);
-    if (roll === 1){
-      alert("Turns up!")
-      return player2Rolls = []
+    console.log("winner exists? " + game.checkForWin())
+    if (game.checkForWin()) {
+      game.endGame(game.winner)
     } else {
-      (player2Rolls).push(roll);
-      sumPlayer2();
+      game.changeTurn()
     }
+    self.turnSpan.text("");
+    self.rollSpan.text("");
+    self.rolls = [];
   })
+}
+function Game() {
+  this.winningScore = 10;
+  this.players = [];
+  this.turn = 0;
+  this.winner = undefined;
+  this.changeTurn = function(noAdvance) {
 
-  $("#player-1-hold-button").click(function(){
-    totalPlayer1()
-
-  })
-
-  $("#player-2-hold-button").click(function(){
-    totalPlayer2()
-  })
-})
+    if (!noAdvance) {
+      if (this.turn === this.players.length-1) {
+        this.turn = 0
+      } else {
+        this.turn++
+      }
+    }
+    console.log("turn " + this.turn)
+    var activePlayer = this.players[this.turn]
+    this.players.forEach(function(player,i){
+      if (activePlayer !== player) {
+        player.rollButton.addClass('obscured')
+        player.holdButton.addClass('obscured')
+        player.avatar.addClass('obscured')
+      } else {
+        activePlayer.rollButton.removeClass('obscured')
+        activePlayer.holdButton.removeClass('obscured')
+        player.avatar.removeClass('obscured')
+      }
+    })
+  }
+  this.checkForWin = function() {
+    var hasWinner = false;
+    this.players.forEach(function(player,i){
+      console.log(player + " checking");
+      console.log(player.score);
+      console.log("to win: " + this.winningScore);
+      if (player.score >= this.winningScore) {
+        this.winner = player;
+        hasWinner = true;
+      }
+    },this)
+    return hasWinner;
+  }
+  this.endGame = function(winner) {
+    $("#winner-name").html("Player " + (winner.index+1));
+    $("#winner-image").html('<img id="player-'+(winner.index+1)+'-win-image" src="'+winner.winImage+'">');
+    $("#win-screen").removeClass("tiny");
+    $("#win-screen").removeClass("obscured");
+    $("#title-header").addClass('off-top');
+    $(".main-area").animate({
+      opacity: 0
+    },1000)
+  }
+}
+function getSum(total, num) {
+  return total + num;
+}
